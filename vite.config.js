8bd7a8f68b0fa799ffc9f39ef7c9f1aa61831a45
elmarce <elmarce@hotmail.de>
2025-10-27 13:49:54 +0100

    fix ziggy route problem

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
     plugins: [
@@ -10,6 +11,11 @@ export default defineConfig({
         }),
         react(),
     ],
+    resolve: {
+        alias: {
+            'ziggy-js': fileURLToPath(new URL('./vendor/tightenco/ziggy/dist/index.es.js', import.meta.url)),
+        },
+    },
     server: {
         host: '127.0.0.1', // Force IPv4 instead of IPv6 [::1]
         strictPort: false,
