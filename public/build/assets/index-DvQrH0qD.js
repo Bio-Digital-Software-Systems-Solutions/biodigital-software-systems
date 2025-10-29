7c316e4681cdcabe26977e463a1edc37a0eb0e73
elmarce <elmarce@hotmail.de>
2025-10-29 15:31:15 +0100

    rebuild

--- public/build/assets/index-DvQrH0qD.js
+++
@@ -1,1 +0,0 @@
-import{g as r,n as t}from"./app-BOwk3ujH.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ public/build/assets/index-HKC-tn-o.js
@@ -0,0 +1,1 @@
+import{g as r,n as t}from"./app-U9GE8rVO.js";var a=t();const e=r(a);export{e as R,a as r};
