ffecc3ce2c14e3ad21c3da7f493f024ba8101825
elmarce <elmarce@hotmail.de>
2026-02-05 14:03:27 +0100

    rebuild all

--- public/build/assets/index-DLfCqfNh.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-CxeA3Nx6.js";var a=globalThis?.document?t.useLayoutEffect:()=>{};export{a as u};
---
+++ public/build/assets/index-DcIy4GT6.js
@@ -0,0 +1,1 @@
+import{r}from"./index-DsGi-gTB.js";var i=r();export{i as s};
